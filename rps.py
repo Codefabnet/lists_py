3c89063c462d4b0204e781922167520bdf3115c8
Steve Browning <spb@codefab.net>
2024-06-03 08:38:49 -0700

    rps: wip.

--- rps.py
+++ rps.py
@@ -1,39 +1,61 @@
 import sys
 import random
+from enum import Enum
 
+class RPS(Enum):
+    ROCK = 0
+    PAPER = 1
+    SCISSORS = 2
+    VALID_MOVES = 3
+
+# valid moves array, including capitalized value,
+# normalized using MOD division.
 moves = ["rock", "paper", "scissors", "Rock", "Paper", "Scissors"]
 
-print()
+print("*********************************")
 
 if len(sys.argv) > 1:
+    # Player command line argument.
     player_input = sys.argv[1]
+
     if len(sys.argv) == 3:
+        # House command line argument, for testing.
         house_input = sys.argv[2]
+
+        # Check for valid input.
         if house_input not in moves:
             sys.exit(f"Incorrect move: {house_input}")
 
-        house_choice = moves.index(house_input) % 3
+        # Choice is an integer index into the moves array.
+        house_choice = moves.index(house_input) % RPS.VALID_MOVES.value
     else:
+        # Random house input if none given.
         house_choice = int(random.choice("012"))
         house_input = moves[house_choice]
 
 else:
-    player_input = "abcdefghijklmnopqrstuvwxyz"
+    # Default player input.
+    player_input = "rock"
 
+# Check for valid input.
 if player_input not in moves:
     sys.exit(f"Incorrect move: {player_input}")
 
-player_choice = moves.index(player_input) % 3
+# Choice is an integer index into the moves array.
+player_choice = moves.index(player_input) % RPS.VALID_MOVES.value
+
+# Print the player/house choices and for now, the normalized int value.
 print(f"Player selects: {player_input} - {player_choice}")
+print(f"House  selects: {house_input} - {house_choice}\n")
 
-print(f"House  selects: {house_input} - {house_choice}")
-
-if ((player_choice == 0 and house_choice == 2) or
-    (player_choice == 1 and house_choice == 0) or
-    (player_choice == 2 and house_choice == 1)):
-   print("player wins")
+# Determine winner, fall through to house wins if other conditions fail.
+if ((player_choice == RPS.ROCK .value and house_choice == RPS.SCISSORS.value) or
+    (player_choice == RPS.PAPER.value and house_choice == RPS.ROCK.value) or
+    (player_choice == RPS.SCISSORS.value and house_choice == RPS.PAPER.value)):
+   print("Player wins".center(30, "="))
 elif player_choice == house_choice:
-    print("tie game")
+    print("Tie game".center(30, "="))
 else:
-    print("house wins")
+    print("House wins".center(30, "="))
     
+print()
