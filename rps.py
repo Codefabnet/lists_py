2d6f95972027c74094f877561d6c922e09c89b1a
Steve Browning <spb@codefab.net>
2024-06-02 20:40:00 -0700

    rps: wip.

--- rps.py
+++ rps.py
@@ -9,6 +9,9 @@ if len(sys.argv) > 1:
     player_input = sys.argv[1]
     if len(sys.argv) == 3:
         house_input = sys.argv[2]
+        if house_input not in moves:
+            sys.exit(f"Incorrect move: {house_input}")
+
         house_choice = moves.index(house_input) % 3
     else:
         house_choice = int(random.choice("012"))
@@ -20,17 +23,14 @@ else:
 if player_input not in moves:
     sys.exit(f"Incorrect move: {player_input}")
 
-if house_input not in moves:
-    sys.exit(f"Incorrect move: {house_input}")
-
 player_choice = moves.index(player_input) % 3
 print(f"Player selects: {player_input} - {player_choice}")
 
-print(f"House  selects: {moves[house_choice]} - {house_choice}")
+print(f"House  selects: {house_input} - {house_choice}")
 
-if (player_choice == 0 and house_choice == 2) or \
-   (player_choice == 1 and house_choice == 0) or \
-   (player_choice == 2 and house_choice == 1):
+if ((player_choice == 0 and house_choice == 2) or
+    (player_choice == 1 and house_choice == 0) or
+    (player_choice == 2 and house_choice == 1)):
    print("player wins")
 elif player_choice == house_choice:
     print("tie game")
