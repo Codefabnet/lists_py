21ad9603c2532c6169d0f5ee6980626d4f81ddec
Steve Browning <spb@codefab.net>
2024-06-08 19:59:54 -0700

    lists: work on slice description.

--- lists.py
+++ lists.py
@@ -58,14 +58,28 @@ print("user_list updated through copied list:\n"
 print()
 print()
 
-print("A slice operation creates a new list object")
+print("----------------------------------------------------------") 
+print("For a slice specifier, the right index points just past\n"
+      "the range sought, or the last item if not present.\n\n"
+      "Using negative numbers, where x = number of items.\n"
+      "The left index ranges from -x to -1, (first to last))\n"
+      "The right index ranges from -(x-1) to no index given.\n\n"
+      "[x:y] == from item at index x to item before index y\n"
+      "[x:] == from item at index x to last item.\n"
+      "[:y] == from first item to item before index y.\n"
+      "[:] == full list, (first to last).\n")
+
+print("A slice operation creates a new list object\n"
+      "new_list = list[x:y], new_list may just be a print param.")
 print("----------------------------------------------------------") 
 full_slice = user_list[:]
 
-print("Slice of user_list, full slice:\n"
+print("Slice of user_list, full slice: [:]\n"
       "                    ", full_slice)
-print("Slice of user_list from second to last item:\n"
+print("Slice of user_list from second to last item: [-2:]\n"
       "                    ", full_slice[-2:])
+print("Slice of user_list from first to second to last item: [-4:-1]\n"
+      "                    ", full_slice[-4:-1])
 full_slice[-1] = 'Stephen'
 print("full slice modified:\n"
       "                    ", full_slice)
@@ -75,18 +89,21 @@ print("user_list unmodified:\n"
 print()
 print()
 
-print("Using a slice to change or clear list items.")
+print("Using a slice to change or clear existing list items.\n"
+      "list[x:y] = modifying_list[x:y]")
 print("----------------------------------------------------------") 
 print("user_list: ".ljust(25), user_list)
 user_list[2:5] = ['Tom', 'Dick', 'Harry']
-print("Changed user_list by slice:\n"
+print("Changed user_list by slice: [2:5]\n"
        "                         ", user_list)
 user_list[1:3] = []
-print("Removed items by slice:\n"
+print("Removed items by slice: [1:3] = []\n"
        "                         ", user_list)
 user_list[:] = []
-print("Clear lsit by slice:\n"
+print("Clear user_list by slice: [:] = []\n"
        "                         ", user_list)
+print("An empty list is logically false:\n"
+       "                          user_list ==", bool(user_list))
 user_list[:] = full_slice
 print("Restore user_list by slice, using full_slice list:\n"
        "                         ", user_list)
@@ -94,3 +111,15 @@ print("Restore user_list by slice, using full_slice list:\n"
 print()
 print()
 
+
+print("Nesting lists.")
+print("----------------------------------------------------------") 
+a = [1, 2, 3, 4]
+b = [0, 1, 2, 3]
+c = [a, b]
+
+print("A list of lists:".ljust(30), c)
+print("First list item in outer list ".ljust(31), c[0])
+print("Second list item in outer list ".ljust(45), c[1])
+print("First item in first list".ljust(32), c[0][0])
+print("First item in second list".ljust(46), c[1][0])
